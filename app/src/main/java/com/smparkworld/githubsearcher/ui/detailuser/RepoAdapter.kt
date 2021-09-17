c73fb6f25dd35fe43b256cb2218e37c90330a0e2
ParkSM <park97.sm@gmail.com>
2021-09-18 01:31:56 +0900

    Layout 안에 있는 상수 값을 dimen과 string으로 변경

--- app/src/main/java/com/smparkworld/githubsearcher/ui/detailuser/RepoAdapter.kt
+++ app/src/main/java/com/smparkworld/githubsearcher/ui/detailuser/RepoAdapter.kt
@@ -4,13 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.smparkworld.githubsearcher.databinding.ItemDetailuserRepoAdapterBinding
+import com.smparkworld.githubsearcher.databinding.ItemDetailuserAdapterRepoBinding
 import com.smparkworld.githubsearcher.model.Repo
 
 class RepoAdapter : ListAdapter<Repo, RepoAdapter.RepoViewHolder>(Repo.DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RepoViewHolder(
-            ItemDetailuserRepoAdapterBinding.inflate(
+            ItemDetailuserAdapterRepoBinding.inflate(
                     LayoutInflater.from(parent.context), parent, false
             )
     )
@@ -20,7 +20,7 @@ class RepoAdapter : ListAdapter<Repo, RepoAdapter.RepoViewHolder>(Repo.DIFF_CALL
     }
 
     class RepoViewHolder(
-            private val binding: ItemDetailuserRepoAdapterBinding
+            private val binding: ItemDetailuserAdapterRepoBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Repo) {
